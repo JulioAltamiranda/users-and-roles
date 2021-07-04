df92b8ac549f7054332fb156da018c011a780e26
julioous <julioaltamiranda77@gmail.com>
2021-07-04 14:18:05 -0300

    Personalizar vistas de errores 403, 403

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,8 @@
+@extends('errors.layout')
+@section('content')
+<div class="text-center">
+    <h1 class="text-white display-1 font-weight-bold">403</h1>
+    <p class="text-white mt-4 mb-4"><strong>Acceso denegado!</strong><br>  No tienes permiso para esta solicitud.</p>
+    <a href="{{url()->previous()}}" class="btn btn-outline-warning">Regresar</a>
+</div>
+@endsection
