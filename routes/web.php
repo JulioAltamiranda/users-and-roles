9bbbbea34ac5ee0acd6480cf4e20b7a7ce2e94dd
julioous <julioaltamiranda77@gmail.com>
2021-07-03 22:27:11 -0300

    \se cambio el redireccionamiento despues de hacer login

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('login');
 });
 
 Auth::routes(['reset' => false]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', 'Admin\DashboardController@index')->name('admin.dashboard');
+});
